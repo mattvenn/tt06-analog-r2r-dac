de432a9f3e6a5d6968d35bd4993a00584a61f2e3
Matt Venn <matt@mattvenn.net>
2024-02-29 15:56:34 +0100

    working extraction script

--- mag/extract_forsim.tcl
+++ mag/extract_forsim.tcl
@@ -1,16 +1,17 @@
-load r2r.mag
 load tt_um_mattvenn_r2r_dac.mag
-extract do resistance
+flatten tt_um_flat
+load tt_um_flat
+select top cell
+cellname delete tt_um_mattvenn_r2r_dac
+cellname rename tt_um_flat tt_um_mattvenn_r2r_dac
 extract all
 ext2sim labels on
 ext2sim
 extresist tolerance 10
-extresist all
+extresist
 ext2spice lvs
-ext2spice cthresh 0.01
-ext2spice rthresh 10
+ext2spice cthresh 0
 ext2spice extresist on
-ext2spice resistor tee on
-ext2spice format ngspice
+ext2spice
 ext2spice -d -o tt_um_mattvenn_r2r_dac.sim.spice
 quit -noprompt
