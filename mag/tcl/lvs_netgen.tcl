bdb65faa38b51ae8c766597e59ea8ed71d5191ca
Matt Venn <matt@mattvenn.net>
2024-03-15 14:04:11 +0100

    add the extra spice and gl verilog files to lvs script

--- mag/tcl/lvs_netgen.tcl
+++ mag/tcl/lvs_netgen.tcl
@@ -1,6 +1,10 @@
 set layout [readnet spice $project.lvs.spice]
 set source [readnet spice /dev/null]
 readnet spice $::env(PDK_ROOT)/$::env(PDK)/libs.ref/sky130_fd_sc_hd/spice/sky130_fd_sc_hd.spice $source
-#readnet spice ../xschem/simulation/$project.spice $source
-readnet verilog ../src/project.v $source
+# add any spice files of your analog blocks:
+readnet spice ../xschem/simulation/r2r.spice $source
+# add an GL verilog of your digital blocks:
+readnet verilog ../verilog/gl/r2r_dac_control.v $source
+# top level GL verilog
+readnet verilog ../verilog/gl/$project.v $source
 lvs "$layout $project" "$source $project" $::env(PDK_ROOT)/sky130A/libs.tech/netgen/sky130A_setup.tcl lvs.report -blackbox
